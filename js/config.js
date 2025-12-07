19395dd40b5148689c9973f3dd9779192c2b6c60
bioenga7md3del <bioenga7md3del@gmail.com>
2025-12-07 14:17:42 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
+const firebaseConfig = {
+    apiKey: "AIzaSyAP-wgrP2o6ifi3XuaMK-lBB43nvrs3GfA",
+    authDomain: "tabuk-kpi.firebaseapp.com",
+    projectId: "tabuk-kpi",
+    storageBucket: "tabuk-kpi.firebasestorage.app",
+    messagingSenderId: "537604438384",
+    appId: "1:537604438384:web:4391b219241c54f7d6f7b5",
+    measurementId: "G-9FCG4452CQ"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// تصدير المتغيرات لنستخدمها في الملفات الأخرى
+export { db };
